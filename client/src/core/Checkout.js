fa33bb7c3741e721548c2d3bb5d0dd61eada19b0
Mladen Bajic <56607388+BajicMladen@users.noreply.github.com>
2020-09-16 15:30:59 +0200

    credit card and paypal paying added

--- client/src/core/Checkout.js
+++ client/src/core/Checkout.js
@@ -2,11 +2,13 @@ import React, { useState, useEffect } from "react";
 import Layout from "./Layout";
 import { isAuthenticated } from "../auth/index";
 import { Link } from "react-router-dom";
-import { getBraintreeClientToken, getProducts } from "./apiCore";
+import { getBraintreeClientToken, processPayment } from "./apiCore";
 import DropIn from "braintree-web-drop-in-react";
+import { emptyCart } from "./cartHelpers";
 
 const Checkout = ({ products }) => {
   const [data, setData] = useState({
+    loading: false,
     success: false,
     clientToken: null,
     error: "",
@@ -22,7 +24,7 @@ const Checkout = ({ products }) => {
       if (data.err) {
         setData({ ...data, error: data.error });
       } else {
-        setData({ ...data, clientToken: data.clientToken });
+        setData({ clientToken: data.clientToken });
       }
     });
   };
@@ -47,25 +49,98 @@ const Checkout = ({ products }) => {
     );
   };
 
+  const buy = () => {
+    //send nonce to server, nonce - data.instanc.req.PaymentMethod()
+    setData({ loading: true });
+
+    let nonce;
+    let getNonce = data.instance
+      .requestPaymentMethod()
+      .then((data) => {
+        //console.log(data);
+        nonce = data.nonce;
+
+        const paymentData = {
+          paymentMethodNonce: nonce,
+          amount: getTotal(products),
+        };
+
+        processPayment(userId, token, paymentData)
+          .then((response) => {
+            setData({ ...data, success: response.success });
+            //empty cart
+            emptyCart(() => {
+              console.log("Payment success and empty cart");
+              setData({ loading: false });
+            });
+            //create order
+          })
+          .catch((error) => {
+            console.log(error);
+            setData({ loading: false });
+          });
+      })
+      .catch((error) => {
+        //console.log("dropin error", error);
+        setData({ ...data, error: error.message });
+      });
+  };
+
   const showDropIn = () => {
     return (
-      <div>
+      <div onBlur={() => setData({ ...data, error: "" })}>
         {data.clientToken !== null && products.length > 0 ? (
           <div>
             <DropIn
-              options={{ authorization: data.clientToken }}
+              options={{
+                authorization: data.clientToken,
+                paypal: {
+                  flow: "vault",
+                },
+              }}
               onInstance={(instance) => (data.instance = instance)}
             ></DropIn>
-            <button className="btn btn-success">Checkout</button>
+            <button onClick={buy} className="btn btn-success btn-block">
+              Pay
+            </button>
           </div>
         ) : null}
       </div>
     );
   };
 
+  const showError = (error) => {
+    return (
+      <div
+        className="alert alert-danger"
+        style={{ display: error ? "" : "none" }}
+      >
+        {error}
+      </div>
+    );
+  };
+
+  const showSuccess = (success) => {
+    return (
+      <div
+        className="alert alert-info"
+        style={{ display: success ? "" : "none" }}
+      >
+        Thank you player ! Transaction was succesifull!
+      </div>
+    );
+  };
+
+  const showLoading = (loading) => {
+    return loading && <h2>Loading</h2>;
+  };
+
   return (
     <div>
       <h2>Total: €{getTotal()}</h2>
+      {showLoading(data)}
+      {showError(data.error)}
+      {showSuccess(data.success)}
 
       {showCheckout()}
     </div>
