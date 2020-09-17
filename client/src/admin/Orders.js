b4eb5d44a6d49e2ad9760abc45b6845f3fb6cb3f
Mladen Bajic <56607388+BajicMladen@users.noreply.github.com>
2020-09-17 20:04:27 +0200

    Change status of order form admin dashboard done

--- client/src/admin/Orders.js
+++ client/src/admin/Orders.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
 import Layout from "../core/Layout";
 import { isAuthenticated } from "../auth/index";
-import { listOrders } from "./apiAdmin";
+import { listOrders, getStatusValues, updateOrderStatus } from "./apiAdmin";
 import moment from "moment";
 
 const Orders = () => {
   const [orders, setOrders] = useState([]);
+  const [statusValues, setStatusValues] = useState([]);
 
   const { user, token } = isAuthenticated();
 
@@ -19,8 +20,19 @@ const Orders = () => {
     });
   };
 
+  const loadStatusValues = () => {
+    getStatusValues(user._id, token).then((data) => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        setStatusValues(data);
+      }
+    });
+  };
+
   useEffect(() => {
     loadOrders();
+    loadStatusValues();
   }, []);
 
   const showOrdersLength = (orders) => {
@@ -49,6 +61,33 @@ const Orders = () => {
     );
   };
 
+  const handleStatusChange = (e, orderId) => {
+    updateOrderStatus(user._id, token, orderId, e.target.value).then((data) => {
+      if (data.error) {
+        console.log("Status update failed");
+      } else {
+        loadOrders();
+      }
+    });
+  };
+
+  const showStatus = (o) => (
+    <div className="form-group">
+      <h3 className="mark mb-4">Status: {o.status}</h3>
+      <select
+        className="form-control"
+        onChange={(e) => handleStatusChange(e, o._id)}
+      >
+        <option>UpdateStatus</option>
+        {statusValues.map((status, index) => (
+          <option key={index} value={status}>
+            {status}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+
   return (
     <Layout
       title="Orders"
@@ -71,7 +110,7 @@ const Orders = () => {
                 </h2>
 
                 <ul className="list-group mb-2">
-                  <li className="list-group-item">Status: {order.status}</li>
+                  <li className="list-group-item">{showStatus(order)}</li>
                   <li className="list-group-item">
                     Transaction ID: {order.transaction_id}
                   </li>
--- server/routes/order.js
+++ server/routes/order.js
@@ -3,7 +3,13 @@ const router = express.Router();
 
 const { userById, addOrderToUserHistory } = require("../controllers/user");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
-const { create, listOrders } = require("../controllers/order");
+const {
+  orderById,
+  create,
+  listOrders,
+  getStatusValues,
+  updateOrderStatus,
+} = require("../controllers/order");
 const { decreaseQuantity } = require("../controllers/product");
 
 router.post(
@@ -16,7 +22,23 @@ router.post(
 );
 
 router.get("/order/list/:userId", requireSignin, isAuth, isAdmin, listOrders);
+router.get(
+  "/order/status-values/:userId",
+  requireSignin,
+  isAuth,
+  isAdmin,
+  getStatusValues
+);
+
+router.put(
+  "/order/:orderId/status/:userId",
+  requireSignin,
+  isAuth,
+  isAdmin,
+  updateOrderStatus
+);
 
 router.param("userId", userById);
+router.param("orderId", orderById);
 
 module.exports = router;
